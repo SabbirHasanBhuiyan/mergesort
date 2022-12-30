7b4a4c48139bbdf447dc9d4b08c6c73f2e5e5d21
SabbirHasanBhuiyan <sabbirhasan675@gmail.com>
2022-12-30 20:06:32 +0600

    Edited One

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -1,44 +1,45 @@
 #include<iostream>
 using namespace std;
 
-void merge (int arr[],int l,int r,int mid)
+void merge (int arr[],int l,int mid,int r)
 {
     int n1 = mid-l+1;
     int n2 = r-mid;
     int a[n1];
     int b[n2];//temp array
 
-    for(int i=0;i<=n1;i++){
+    for(int i=0;i<n1;i++){
         a[i]=arr[l+i];
     }
-    for(int i=0;i<=n2;i++){
-        a[i]=arr[mid+1+i];
+    for(int i=0;i<n2;i++){
+        b[i]=arr[mid+1+i];
     }
 
     int i=0;
     int j=0;
     int k=l;
+
     while (i<n1 && j<n2)
     {
-        if (a[i],b[j])
+        if (a[i]<=b[j])
         {
              arr[k]=a[i];
+             k++; i++;
+        }else{
+             arr[k]=b[j];
              k++; j++;
         }
     }
 
     while (i<n1){
         arr[k]=a[i];
-        k++; j++;
+        k++; i++;
     }
-    while (i<n2){
+    while (j<n2){
         arr[k]=b[j];
         k++; j++;
     }
 
-    {
-        /* code */
-    }
 
 }
 
@@ -69,7 +70,7 @@ int main()
         cin>>arr[q];
     }
     mergeSort(arr,0,p-1);//printing the sorted data.
-    cout<<"\nSorted data";
+    cout<<"\nSorted data\n";
     for(q=0;q<p;q++)
     {
         cout<<arr[q]<<" ";
